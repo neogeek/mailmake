3ddd6a751f548fc5c4d2a899284d07989e9886c8
Scott Doxey <neogeek0101@gmail.com>
2020-09-11 01:20:30 -0400

    Formatted files.

--- test/specs/mailmake.js
+++ test/specs/mailmake.js
@@ -5,89 +5,79 @@ const fs = require('fs');
 const mailmake = require('../../lib/mailmake');
 
 describe('mailmake', () => {
-
     before(() => {
-
         mailmake.seed = 's';
-
     });
 
     it('should generate random string', () => {
-
         assert.equal(mailmake.randomString(), 'ssssssssssssssssssss');
-
     });
 
     it('should generate boundary', () => {
-
-        assert.equal(mailmake.boundaryString(), '==============ssssssssssssssssssss==');
-
+        assert.equal(
+            mailmake.boundaryString(),
+            '==============ssssssssssssssssssss=='
+        );
     });
 
     it('should generate mime file from file', done => {
+        mailmake
+            .generate('./test/fixtures/email.html', {
+                from: 'test@example.com',
+                subject: 'Hello World!',
+                to: 'mailing-list@example.com'
+            })
+            .then(output => {
+                // fs.writeFileSync('./test/fixtures/email.mime', output, 'utf8');
 
-        mailmake.generate('./test/fixtures/email.html', {
-            'from': 'test@example.com',
-            'subject': 'Hello World!',
-            'to': 'mailing-list@example.com'
-        }).then(output => {
+                fs.readFile(
+                    './test/fixtures/email.mime',
+                    'utf8',
+                    (err, data) => {
+                        assert.equal(output, data);
 
-            // fs.writeFileSync('./test/fixtures/email.mime', output, 'utf8');
-
-            fs.readFile('./test/fixtures/email.mime', 'utf8', (err, data) => {
-
-                assert.equal(output, data);
-
-                done();
-
+                        done();
+                    }
+                );
             });
-
-        });
-
     });
 
     it('should generate mime file from string', done => {
-
         fs.readFile('./test/fixtures/email.html', 'utf8', (err, email) => {
+            mailmake
+                .generate(email, {
+                    from: 'test@example.com',
+                    subject: 'Hello World!',
+                    to: 'mailing-list@example.com'
+                })
+                .then(output => {
+                    fs.readFile(
+                        './test/fixtures/email.mime',
+                        'utf8',
+                        (err, data) => {
+                            assert.equal(output, data);
 
-            mailmake.generate(email, {
-                'from': 'test@example.com',
-                'subject': 'Hello World!',
-                'to': 'mailing-list@example.com'
-            }).then(output => {
-
-                fs.readFile('./test/fixtures/email.mime', 'utf8', (err, data) => {
-
-                    assert.equal(output, data);
-
-                    done();
-
+                            done();
+                        }
+                    );
                 });
-
-            });
-
         });
-
     });
 
     it('should generate mime file from string (private method)', done => {
-
         fs.readFile('./test/fixtures/email.html', 'utf8', (err, email) => {
-
             fs.readFile('./test/fixtures/email.mime', 'utf8', (err, data) => {
-
-                assert.equal(mailmake.generateFromString(email, {
-                    'from': 'test@example.com',
-                    'subject': 'Hello World!',
-                    'to': 'mailing-list@example.com'
-                }), data);
+                assert.equal(
+                    mailmake.generateFromString(email, {
+                        from: 'test@example.com',
+                        subject: 'Hello World!',
+                        to: 'mailing-list@example.com'
+                    }),
+                    data
+                );
 
                 done();
-
             });
-
         });
-
     });
-
 });
